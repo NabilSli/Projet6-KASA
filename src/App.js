50760e1f9b3b93e068a33a88a539e5d435feb90b
nabil Slimani <slimaninabil93350@gmail.com>
2023-08-28 15:14:32 +0200

    Feat(App.js): add routes for each pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home.page";
+import AboutUs from "./pages/About.page";
+import Error from "./pages/Error.page";
+import Housing from "./pages/Housing.page";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/housing" element={<Housing />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>Home.page</div>
+    <div>Hello world</div>
   )
 }
