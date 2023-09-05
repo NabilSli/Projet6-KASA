641b586a9c64d58eb1b1feb8f52b0d20aa236eb3
nabil Slimani <slimaninabil93350@gmail.com>
2023-09-05 15:57:01 +0200

    feat (gallery.jsx): add fonctions and useEffect to get the data and create a gallery with all the housings

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Card } from "./cards/Cards";
 
+async function getHousing() {
+  try {
+    const fetchResponse = await fetch(`../../assets/data/housingData.json`);
+    const response = await fetchResponse.json();
+    return response;
+  } catch (error) {
+    console.error();
+    return null;
+  }
+}
+
 export default function Gallery() {
+  const [housings, setHousings] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    let ignore = false;
+    getHousing().then((result) => {
+      if (!ignore) {
+        if (typeof result == "object") {
+          setHousings(result);
+        } else {
+          setError("Je ne trouve aucun logement a afficher");
+        }
+      }
+    });
+
+    console.log(housings);
+    console.log(error);
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <section className="gallery">
       <Card title="Titre de la location" link="#" />
