2816eb4d1f12804e6d527ebb8003877fb790f269
nabil Slimani <slimaninabil93350@gmail.com>
2023-09-06 18:43:03 +0200

    feat (cards.jsx): make the image be the background of cards and fit it

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Card = ({ title, image }) => {
   return (
-    <Link className="cardContainer">
-      {image && <img alt={title} src={image}></img>}
-      <div>{title && <h2 className="cardTitle">{title}</h2>}</div>
+    <Link>
+      <div
+        className="cardContainer"
+        style={{
+          background: `linear-gradient(180deg,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%
+), url("${image}")`,
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <div>{title && <h2 className="cardTitle">{title}</h2>}</div>
+      </div>
     </Link>
   );
 };
