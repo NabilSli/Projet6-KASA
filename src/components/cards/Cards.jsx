414328dffc99b86813b48a05a68fb97f018817df
nabil Slimani <slimaninabil93350@gmail.com>
2023-09-06 16:29:02 +0200

    refacto (cards.jsx): reorganise cards html

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export const Card = ({ title, image }) => {
   return (
     <Link className="cardContainer">
-      {title && <h2 className="cardTitle">{title}</h2>}
       {image && <img alt={title} src={image}></img>}
+      <div>{title && <h2 className="cardTitle">{title}</h2>}</div>
     </Link>
   );
 };
