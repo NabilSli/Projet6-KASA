8d409ac1ab6c55ca0e6b5be252bd54ead3aa9f71
nabil Slimani <slimaninabil93350@gmail.com>
2023-09-06 15:12:59 +0200

    chore (cards.jsx): cleaning after pr

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Card = ({ title, link }) => {
+export const Card = ({ title }) => {
   return (
     <Link className="cardContainer">
       {title && <h2 className="cardTitle">{title}</h2>}
