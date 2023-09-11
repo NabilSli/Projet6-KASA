5cf9eeda12eeee7210ae9e5ea40def8b990538c2
nabil Slimani <slimaninabil93350@gmail.com>
2023-09-11 18:25:48 +0200

    chore(gallery.jsx) add a proper loading view

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,12 +3,35 @@ import React, { useState, useEffect } from "react";
 import { Card } from "./cards/Cards";
 import GetData from "././getData";
 
+// TODO: add this and a proper loading view
+//  export default function Gallery() {
+//   const [housings, setHousings] = useState(null);
+//   const [error, setError] = useState(false);
+//   const [isLoading, setLoading] = useState(false);
+//   useEffect(() => {
+//     let ignore = false;
+//     setLoading(true);
+//     getHousing().then((result) => {
+//       setLoading(false);
+//       if (!ignore) {
+//         if (typeof result == "object") {
+//           setHousings(result);
+//         } else {
+//           setError("Je ne trouve aucun logement a afficher");
+//         }
+//       }
+//     });
+//     return () => {
+//       ignore = true;
+//     };
+//    }, [])
+
 // NOTE: using useEffect to catch errors of fetch and have an error message show up for better UI
 export default function Gallery() {
   const [housings, setHousings] = useState(null);
   const [error, setError] = useState(false);
 
-  // TODO: abstarct this into a hook
+  // TODO: abstarct this into a hook and add .catch for errors
   useEffect(() => {
     let ignore = false;
     GetData().then((result) => {
