433a349349f2df40ebee403bb69b77129f907777
nabil Slimani <slimaninabil93350@gmail.com>
2023-09-12 15:32:22 +0200

    chore (Cards.jsx): handle errors for the background images

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import fallbackImage from "../../assets/images/titleBackground.svg";
+
 export const Card = ({ title, image }) => {
+  const backgroundImage =
+    typeof image == "string" && image.length > 0 ? image : fallbackImage;
+
   return (
     <Link to={"/housing"}>
       <div
         className="cardContainer"
         style={{
           background: `linear-gradient(180deg,rgba(255, 255, 255, 0) 0%,rgba(0, 0, 0, 0.5) 100%
-), url("${image}")`,
+), url("${backgroundImage}")`,
           backgroundSize: "100% 100%",
         }}
       >
