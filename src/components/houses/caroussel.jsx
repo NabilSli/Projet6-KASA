65102211494b77456b4ea64a6388c113eab97a7e
nabil Slimani <slimaninabil93350@gmail.com>
2023-10-09 16:08:55 +0200

    chore (carousel.jsx): show caroussel arrow boutons and counter only if there is more then 1 picture

--- src/components/houses/caroussel.jsx
+++ src/components/houses/caroussel.jsx
@@ -19,25 +19,34 @@ const Carousel = ({ pictures }) => {
   return (
     <div className="carousel">
       <div className="image-container">
-        {""}
-        <button onClick={prevImage} className="arrowLeft">
-          <img
-            src={arrowLeft}
-            alt="previous button"
-            className="arrowLeft"
-          ></img>
-        </button>
+        {pictures.length > 1 && (
+          <button onClick={prevImage} className="arrowLeft">
+            <img
+              src={arrowLeft}
+              alt="previous button"
+              className="arrowLeft"
+            ></img>
+          </button>
+        )}
         <img
           className="carousselPictures"
           src={pictures[currentIndex]}
           alt={`pictures ${currentIndex + 1}`}
         />
-        <button onClick={nextImage} className="arrowRight">
-          <img src={arrowRight} alt="next button" className="arrowRight"></img>
-        </button>
-        <div className="counter">{`${currentIndex + 1}/${
-          pictures.length
-        }`}</div>
+        {pictures.length > 1 && (
+          <button onClick={nextImage} className="arrowRight">
+            <img
+              src={arrowRight}
+              alt="next button"
+              className="arrowRight"
+            ></img>
+          </button>
+        )}
+        {pictures.length > 1 && (
+          <div className="counter">{`${currentIndex + 1}/${
+            pictures.length
+          }`}</div>
+        )}
       </div>
     </div>
   );
